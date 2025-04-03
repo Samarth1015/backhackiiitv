a436e17ae30ceecef7fca741085c0c02694cea37
samarth <202351125@iiitvadodara.ac.in>
2025-04-04 00:51:22 +0530

    sa

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const Team = require("./model/schema");
 const cors = require("cors");
-dotenv.config();
 
+dotenv.config();
 const app = express();
+
 const db = process.env.PSWD;
 app.use(cors());
 
 mongoose
-  .connect(db)
-  .then(() => console.log("Connection successful"))
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Database connection error:", err));
 
 app.use(express.json());
@@ -19,19 +23,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post("/api", async (req, res) => {
   try {
-    console.log("Request received");
-
     const { teamname } = req.body;
-    console.log(teamname);
-
     const newTeam = new Team({ teamname });
     await newTeam.save();
-
-    res.status(201).json({ message: "Team saved successfully", team: newTeam });
+    res
+      .status(201)
+      .json({ message: "Team saved successfully!", team: newTeam });
   } catch (error) {
-    console.error("Error saving team:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-app.listen(3000, () => console.log("gg server is running on port 3000"));
+module.exports = app;
